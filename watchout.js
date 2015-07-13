fdbedc03b654cf3f5f4086dbc847f0b42caf6eea
Chun Hao Wang <chunhao137@gmail.com>
2015-07-12 19:57:42 -0700

    new commit

--- watchout.js
+++ watchout.js
@@ -1,45 +1,117 @@
-// start slingin' some d3 here.
-d3.select('.current').select('span').text('number')
 
-var dataArray = d3.range(30)
-var width = window.innerWidth;
+var enemy = d3.range(20); // creates an array from 1 to x
 var height = window.innerHeight;
-
-
-var color = d3.scale.linear()
-			.domain([10,50])
-			.range(["red","blue"])
-
+var width = window.innerWidth;
+var score = 0;
+var highScore = 0;
+var counter  = function(){
+  var count =0;
+  setInterval(function(){
+    score++;
+    d3.select('.current').select('span').text(score);
+    if(collision()===true){
+      count++;
+      d3.select('.collisions').select('span').text(count);
+      if(score>highScore){
+        highScore = score;
+      }
+    score = 0;
+    d3.select('.high').select('span').text(highScore);
+    }
+  }, 10);
+}();
 
 
 var canvas = d3.select('body')
-			.append('svg')
-			.attr('width',width)
-			.attr('height',height)
-			.append('g')
-			.attr("transform", "translate(40,40)")
+                .append('svg')
+                .attr('height', height)
+                .attr('width', width);
+
+var loop =  function(){
+
+                d3.select(this)
+                .transition().duration(1000)
+                .attr('cx', function(value){
+                  return Math.random() * width;
+                })
+                .attr('cy', function(value){
+                  return Math.random() * height;
+                })
+
+                .each("end",loop);
+
+  }
+
+  var enemyCircle = canvas.selectAll('circle')
+                  .data(enemy)
+                  .enter()
+                  .append('circle')
+                  .attr('class','enemy')
+                  .attr('cx', function(value){
+                    return Math.random() * width;
+                  })
+                  .attr('cy', function(value){
+                    return Math.random() * height;
+                  })
+                  .attr('r', function(value){return Math.random()*25})
+                  .attr('fill', 'blue')
+
+                  .transition()
+                  .duration(1000)
+                  .attr('cx', function(value){
+                    return Math.random() * width;
+                  })
+                  .attr('cy', function(value){
+                    return Math.random() * height;
+                  }).each("end",loop);
 
 
-var bars = canvas.selectAll('circle')
-			.data(dataArray)
-			.enter()
-			.append('circle')
-			.attr('cx',function(val){return Math.random()*width})
-			.attr('r', 10)
-			.attr('fill', function(val){return color(val)})
-			.attr('cy', function(val){return Math.random()*height});
 
-// var bubbles = canvas.selectAll('circle')
-// .data(array)
-// .enter()
-// .append('circle')
-// .attr('cx',250)
-// .attr('cy',250)
-// .attr('fill','green')
-// .attr('r', function(val){return vall})
+var drag = d3.behavior.drag()
+    .on("drag", dragmove);
+
+function dragmove(d) {
+    var x = d3.event.x;
+    //console.log(x);
+    var y = d3.event.y;
+    d3.select(this).attr("cx", x);
+    d3.select(this).attr("cy", y);
+}
 
 
 
+var playerCircle =  d3.select('svg')
+                    .data([10])
+                    .append('circle')
+                    .attr('class','player')
+                    .attr('cx', 200)
+                    .attr('cy', 500)
+                    .attr('r', 20)
+                    .attr('fill', 'red')
+                    .attr("stroke", "orange")
+                    .attr("stroke-width", 10)
+                    .call(drag)
 
 
+var collision = function(){
 
+
+  var playerX =  d3.select('.player').attr('cx');
+  var playerY =  d3.select('.player').attr('cy');
+
+
+  var enemies = canvas.selectAll('circle').data(enemy)[0];
+
+  for(var i=0; i<enemies.length; i++){
+
+    var enemyX = enemies[i].cx.animVal.value;
+    var enemyY = enemies[i].cy.animVal.value;
+    var distance = Math.sqrt(Math.pow(playerX-enemyX,2)+ Math.pow(playerY-enemyY,2));
+    if(distance < 50 ){
+
+      return true;
+    }else{
+
+    }
+  }
+};
